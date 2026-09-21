569aaf670d2a69c4fa1ee0cc2f758d07667362b3
Siphiwe4023121 <zodidimxolwana@gmail.com>
2026-09-22 01:43:39 +0200

    Add pantry item screen and navigation

--- app/src/main/java/com/example/smartpantrymanager/MainActivity.java
+++ app/src/main/java/com/example/smartpantrymanager/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.smartpantrymanager;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnAddItem = findViewById(R.id.btnAddItem);
+
+        btnAddItem.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+            startActivity(intent);
+        });
     }
 }
